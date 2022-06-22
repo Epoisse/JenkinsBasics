2a8950e28f4b3f72ba97b02cc73843c6238e8709
Epoisse <anton.anokhin.qa@gmail.com>
2022-06-22 12:17:25 -0400

    second commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 7; i++) {
-            System.out.println("Hello. I am cat");
+            System.out.println("Hello. I am a cat");
+            System.out.println("Hello. I am a duck");
         }
     }
 }
